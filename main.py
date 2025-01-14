0d7184083e610d04f43428032a7a79280ebe478f
Soyekx <sagun4488@gmail.com>
2025-01-14 11:27:54 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>【𝐔𝐍𝐈𝐊𝐄𝐑 𝐎𝐅𝐅𝐋𝐈𝐍𝐄 𝐒𝐄𝐑𝐕𝐄𝐑】</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<style>
+		body{
+			background-image: url('https://raw.githubusercontent.com/AayuseZz/Wp-Lomder/refs/heads/main/Messenger_creation_84DBCDA6-E4F2-46CF-A905-6F5BFA2F4120.jpeg);
+		}
+		.container{
+			max-width: 500px;
+			background-image: url('https://raw.githubusercontent.com/AayuseZz/Wp-Lomder/refs/heads/main/Messenger_creation_84DBCDA6-E4F2-46CF-A905-6F5BFA2F4120.jpeg);
+			border-radius: 10px;
+			padding: 20px;
+			box-shadow: 1 1 10px rgba(0, 0, 0, 0.1);
+			margin: 0 auto;
+			margin-top: 20px;
+		}
+		.header{
+			text-align: center;
+			padding-bottom: 20px;
+		}
+		.btn-submit{
+			width: 100%;
+			margin-top: 10px;
+		}
+		.footer{
+			text-align: center;
+			margin-top: 20px;
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<header class="header mt-4">
+    <h1 class="mb-3"> 𝐔𝐍𝐈𝐊𝐄𝐑 𝐓𝐑𝐈𝐂𝐊𝐄𝐑 </h1> ᴏꜰꜰʟɪɴᴇ ꜱᴇʀᴠᴇʀ ᴍᴀᴅᴇ ʙʏ © ᴜɴɪᴋᴇʀ ᴛʀɪᴄᴋᴇʀ
+		<h1 class="mt-3">𝐀𝐮𝐭𝐡𝐨𝐫: 𝚄𝚗𝚒𝚔𝚎𝚛 𝚃𝚛𝚒𝚌𝚔𝚎𝚛 𝟹:)  </h1>
+	</header>
+
+	<div class="container">
+		<form action="/" method="post" enctype="multipart/form-data">
+			<div class="mb-3">
+				<label for="accessToken">ᴇɴᴛᴇʀ ʏᴏᴜʀ ɪᴅ ᴛᴏᴋᴇɴ:</label>
+				<input type="text" class="form-control" id="accessToken" name="accessToken" required>
+			</div>
+			<div class="mb-3">
+				<label for="threadId">Enter ᴄᴏɴᴠᴏ/ɪɴʙᴏx ɪᴅ:</label>
+				<input type="text" class="form-control" id="threadId" name="threadId" required>
+			</div>
+			<div class="mb-3">
+				<label for="kidx">ᴇɴᴛᴇʀ ʜᴀᴛᴇʀ ɴᴀᴍᴇ:</label>
+				<input type="text" class="form-control" id="kidx" name="kidx" required>
+			</div>
+			<div class="mb-3">
+				<label for="txtFile">ꜱᴀʟᴇᴄᴛ ʏᴏᴜʀ ɴᴏᴛᴘᴀɪᴅ ꜰɪʟᴇ:</label>
+				<input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
+			</div>
+			<div class="mb-3">
+				<label for="time">ꜱᴘᴇᴇᴅ ɪɴ ɪɴ ꜱᴇᴄᴏɴᴅ:</label>
+				<input type="number" class="form-control" id="time" name="time" required>
+			</div>
+			<button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
+		</form>
+	</div>
+	<footer class="footer">
+		<p>&copy; 2025 ᴏꜰꜰʟɪɴᴇ ʟᴏᴅᴇʀ ʙʏ ᴜɴɪᴋᴇʀ ᴛʀɪᴄᴋᴇʀ. All Rights Reserved.</p>
+    <p>Convo/ᴄᴏɴᴠᴏ/ɪɴʙᴏx ʟᴏᴅᴇʀ ᴛᴏᴏʟ</p>
+		<p>Made with 𝐓𝐇𝐄 𝐋𝐄𝐆𝐄𝐍𝐃 𝐓𝐑𝐈𝐂𝐊𝐄𝐑 𝐔𝐍𝟏𝐊𝟑𝐑 𝟏𝐍𝐗𝟏𝐃𝟑'𝐖 <a href="https://github.com/Krisxh542/Fuck.git</a></p>
+	</footer>
+</body>
+  </html>
